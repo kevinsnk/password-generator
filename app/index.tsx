03e48551a561440d14cadf499fed104cc04cf70b
kevinsnk <kevin.santos.solorzano@gmail.com>
2025-05-07 20:02:44 -0600

    :sparkles: Nueva funcionalidad
    Se implementa funcionalidad de generacion de contraseña

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,95 @@
 import Clipboard from '@react-native-clipboard/clipboard';
 import { useState } from "react";
-import { Switch, Text, TextInput, View } from "react-native";
+import { StyleSheet, Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   const [password, setPassword] = useState('');
   const [passwordLenght, setPasswordLenght] = useState('12');
   const [useSymbols, setUseSymbols] = useState(true);
   const [useNumbers, setUseNumbers] = useState(true);
-  const [useLowerCase, setLowerCase] = useState(true);
-  const [useUpperCase, setUpperCase] = useState(true);
+  const [useLowerCase, setUseLowerCase] = useState(true);
+  const [useUpperCase, setUseUpperCase] = useState(true);
   const [successMessage, setSuccessMessage] = useState('');
+
+  const styles = StyleSheet.create({
+
+    container: {
+      margin: 20,
+      marginTop: 100,
+      padding: 20,
+      borderColor: '#ccc',
+      borderRadius: 8,
+      borderWidth: 1,
+      shadowColor: 'grey',
+      shadowOffset: { width: 0, height: 0 },
+      shadowOpacity: 1,
+      shadowRadius: 10,
+      elevation: 5,
+      backgroundColor: '#fff',
+    },
+    header: {
+      color: 'green',
+      textAlign: 'center',
+      fontSize: 30,
+      marginBottom: 10,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 10,
+    },
+    label: {
+      flex: 1,
+      fontSize: 18,
+    },
+    input: {
+      flex: 2,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 8,
+      fontSize: 16,
+    },
+    checkbox:{
+      flex:1,
+      padding: 10,
+    },
+    checkboxLabel: {
+      fontSize: 20,
+      flex:2
+    },
+    button: {
+      padding: 13,
+      backgroundColor: '#007bff',
+      color: '#fff',
+      borderRadius: 5,
+      overflow: 'hidden',
+      textAlign: 'center',
+      fontSize: 16,
+      fontWeight: 'bold',
+      margin: 15,
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    copyButton: {
+      marginLeft: 10,
+      backgroundColor: '#007bff',
+      color: '#fff',
+      borderRadius: 5,
+      overflow: 'hidden',
+      padding: 10,
+      fontSize: 14,
+    },
+    successMessage: {
+      color: 'green',
+      textAlign: 'center',
+      fontSize: 20,
+    }
+  })
 
   const generarPassword = () => {
     let charset = '';
@@ -34,23 +114,62 @@ export default function App() {
   }
 
   return (
-    <View>
-      <Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>
         Password Generator
       </Text>
-      <View>
-        <Text>
-          Tamaño de la contraseña
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>
+          Tamaño de la contraseña:
         </Text>
         <TextInput keyboardType="numeric" value={passwordLenght}
-          onChangeText={(text) => setPasswordLenght(text)} />
+          onChangeText={(text) => setPasswordLenght(text)} style={styles.input} />
       </View>
-      <View>
+      <View style={styles.inputContainer}>
         <Switch value={useSymbols} onValueChange={() => setUseSymbols(!useSymbols)} />
-        <Text>
+        <Text style={styles.checkboxLabel}>
           Symbols
         </Text>
       </View>
+      <View style={styles.inputContainer}>
+        <Switch value={useNumbers} onValueChange={() => setUseNumbers(!useNumbers)} />
+        <Text style={styles.checkboxLabel}>
+          Numbers
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Switch value={useLowerCase} onValueChange={() => setUseLowerCase(!useLowerCase)} />
+        <Text style={styles.checkboxLabel}>
+          LowerCase
+        </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Switch value={useUpperCase} onValueChange={() => setUseUpperCase(!useUpperCase)} />
+        <Text style={styles.checkboxLabel}>
+          UpperCase
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={generarPassword}>
+        <Text style={styles.buttonText}>
+          Generar Contraseña
+        </Text>
+      </TouchableOpacity>
+      {password !== '' && (
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>
+            Contraseña Generada:
+          </Text>
+          <TextInput value={password} style={styles.input} />
+          <TouchableOpacity style={styles.copyButton} onPress={copyToClipboard}>
+            <Text style={styles.buttonText}>
+              Copiar
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {successMessage !== '' && <Text style={styles.successMessage}>
+        {successMessage}
+      </Text>}
     </View>
   )
 }
